2bc74e3c5b198b0d0f4b6d0903e3499b1ca15d2d
Jakub M. Dzik <abukaj@users.noreply.github.com>
2023-04-12 22:17:42 +0200

    2006-08-18 14:18

---
+++ temp.h
@@ -0,0 +1,8 @@
+/*ABUKAJtools - temp.h - v. 0.2 - Jakub Mateusz Kowalski*/
+#define false 0
+#define true (~false)
+
+void clearscreen();
+void mypause();
+int ncsstrcmp(char *, char *);
+int ncsstrncmp(char *, char *, int);
