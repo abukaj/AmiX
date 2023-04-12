13fa2f1d526b4f84d9061f8b9bf2f07418bf3e89
Jakub M. Dzik <abukaj@users.noreply.github.com>
2023-04-12 22:44:37 +0200

    2006-09-04 16:57

--- temp.c
+++ temp.c
@@ -36,7 +36,7 @@ int ncsstrcmp(char *cs, char *ct){
   while (*cs != '\0' && tolower(*cs) == tolower(*ct)){
     cs++; ct++;
     }
-  return *cs - *ct;
+  return tolower(*cs) - tolower(*ct);
   }
 
 /*****************************************************************************\
@@ -47,5 +47,47 @@ int ncsstrncmp(char *cs, char *ct, int n){
   while (*cs != '\0' && tolower(*cs) == tolower(*ct) && --n > 0){
     cs++; ct++;
     }
-  return *cs - *ct;
+  return tolower(*cs) - tolower(*ct);
   }
+
+/*****************************************************************************\
+\*****************************************************************************/
+
+char inttohex(int n){
+  switch (n){
+    case 1:
+      return '1';
+    case 2:
+      return '2';
+    case 3:
+      return '3';
+    case 4:
+      return '4';
+    case 5:
+      return '5';
+    case 6:
+      return '6';
+    case 7:
+      return '7';
+    case 8:
+      return '8';
+    case 9:
+      return '9';
+    case 0:
+      return '0';
+    case 10:
+      return 'A';
+    case 11:
+      return 'B';
+    case 12:
+      return 'C';
+    case 13:
+      return 'D';
+    case 14:
+      return 'E';
+    case 15:
+      return 'F';
+    }
+  return 0;
+  }
+
