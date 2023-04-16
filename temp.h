3cdb3d358d6d56306308aee1146e353d08976012
Jakub M. Dzik <abukaj@users.noreply.github.com>
2023-04-16 17:29:33 +0200

    2006-09-16 17:28

--- temp.h
+++ temp.h
@@ -1,4 +1,4 @@
-/*ABUKAJtools - temp.h - v. 0.2 - Jakub Mateusz Kowalski*/
+/*ABUKAJtools - temp.h - v. 0.3 - Jakub Mateusz Kowalski*/
 /*#define false 0
 #define true (~false)*/
 
@@ -7,3 +7,7 @@ void mypause();
 int ncsstrcmp(char *, char *);
 int ncsstrncmp(char *, char *, int);
 char inttohex(int);
+unsigned char unicode2iso(unsigned int);
+unsigned int iso2unicode(unsigned char);
+unsigned char *iso2utf8string(unsigned char *);
+unsigned char *utf82isostring(unsigned char *);
